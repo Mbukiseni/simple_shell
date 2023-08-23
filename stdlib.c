a94cd98447d3f773cb3227b817ca56333e4c7fc3
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-23 15:47:58 +0100

    Realloc

--- stdlib.c
+++ stdlib.c
@@ -1,65 +1,17 @@
 #include "main.h"
 
 /**
- * _realloc - reallocate memory
- * @ptr: pointer with data
- * @len: lenght of pointer
- * @new_len: new length of allocated memory
- * Return: nothing
- */
-
-void *_realloc(char *ptr, unsigned int len, unsigned int new_len)
-{
-	char *buf;
-	unsigned int i = 0;
-	char *c_ptr;
-
-	if (ptr == NULL)
-	{
-		buf = malloc(new_len);
-		if (buf == NULL)
-			return (NULL);
-
-		return (buf);
-	}
-
-	if (len == new_len)
-		return (ptr);
-
-	if (ptr != NULL && len == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	else
-		c_ptr = ptr;
-
-	buf = malloc(len * sizeof(c_ptr));
-	if (buf == NULL)
-	{
-		free(buf);
-		return (NULL);
-	}
-
-	while (i < len && i < new_len)
-		buf[i++] = *c_ptr++;
-
-	free(ptr);
-	return (buf);
-}
-
-/**
- * copyPtr - copy pointer to fro mbvuffer
+ * copyPtr - copy pointer to buffer and free buffer
  * @ptr: pointer
- * @ptr_len: pointer len
+ * @ptr_len: pointer len (size)
  * @buf: buffer
- * @buf_len: buffer len
+ * @buf_len: buffer len (size)
  * Return: nothing
  */
 
 void copyPtr(char **ptr, size_t *ptr_len, char *buf, size_t buf_len)
 {
-	if (*ptr == NULL || *ptr_len < buf_len)
+	if (*ptr_len < buf_len || *ptr == NULL)
 	{
 		if (buf_len > 32)
 			*ptr_len = buf_len;
@@ -73,3 +25,52 @@ void copyPtr(char **ptr, size_t *ptr_len, char *buf, size_t buf_len)
 		free(buf);
 	}
 }
+
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @len: length of ptr
+ * @new_len: new length of ptr
+ * Return: nothing
+ */
+
+void *_realloc(char *ptr, unsigned int len, unsigned int new_len)
+{
+	unsigned int i = 0;
+	char *buffer;
+	char *char_ptr;
+
+	if (ptr == NULL)
+	{
+		buffer = malloc(new_len);
+		if (buffer == NULL)
+			return (NULL);
+
+		return (buffer);
+	}
+
+	if (len == new_len)
+		return (ptr);
+
+	if (ptr != NULL && len == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+		char_ptr = ptr;
+
+	buffer = malloc(len * sizeof(char_ptr));
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (NULL);
+	}
+
+	while (i < new_len && i < len)
+		buffer[i++] = *char_ptr++;
+
+	free(ptr);
+	return (buffer);
+}
+
