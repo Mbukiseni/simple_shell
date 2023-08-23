5a513b158d5143bae2ae2c015fdb2f0663ca1b65
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-23 14:20:50 +0100

    1

--- buildInFunc.c
+++ buildInFunc.c
@@ -1,26 +1,15 @@
 #include "main.h"
 
-
 /**
- * cdCommand - change directory command - builtin
- * @param: environ and args data.  args[0] = "cd".  args[1] =  directory.
- * Return: 1 on success
+ * hdlCtrlCommand - to handle "^D" key press
+ * @param: eviron and args data
+ * Return: returns 1.
  */
-int cdCommand(data_t *param)
+int hdlCtrlCommand(data_t *param)
 {
-	if (param->args[0] == NULL)
-	{
-		_puts_err(param->av[0]);
-		_puts_err(": expected argument to \"cd\"\n");
-	}
-	else
-	{
-		if (chdir(param->args[1]) != 0)
-			_puts("$ ");
-	}
+	free(param->args);
 	return (1);
 }
-
 
 /**
  * helpCommand - prints help to shell
@@ -34,18 +23,5 @@ int helpCommand(data_t *param)
 	_puts("Sorry we didn't have enough time to write a documentation\n");
 
 	_puts("cd \nhelp\nexit\nenv\n^D\n");
-	return (1);
-}
-
-
-
-/**
- * hdlCtrlCommand - to handle "^D" key press
- * @param: eviron and args data
- * Return: returns 1.
- */
-int hdlCtrlCommand(data_t *param)
-{
-	free(param->args);
 	return (1);
 }
