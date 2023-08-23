7d1d24350aeba33f533edcd2fb06e9d9e27a859b
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-23 16:27:42 +0100

    String Functions

--- string.c
+++ string.c
@@ -1,87 +1,32 @@
 #include "main.h"
 
 /**
- * _strlen - find string length
- * @str: string pointer
+ * _strlen - returns the length of a string
+ * @str: string
  * Return: length of string
  */
 
 size_t _strlen(char *str)
 {
-	size_t size = 0;
+	size_t len = 0;
 
 	if (str != NULL)
-		while (str[size] != '\0')
-			size++;
+		while (str[len] != '\0')
+			len++;
 
-	return (size);
+	return (len);
 }
 
 /**
- * _strcmp - compares two strings
- *
- * @s1: 1st arg - destination string
- *
- * @s2: 2nd arg - source string
- *
- * Return: -15 if s1 is less, 0 if same and 15 is s1 is greaater s2
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0, result = 0, len, s1_len = 0, s2_len = 0;
-
-	s1_len = _strlen(s1);
-	s2_len = _strlen(s2);
-	if (s1_len <= s2_len)
-		len = s1_len;
-	else
-		len = s2_len;
-	while (i <= len)
-	{
-		result = s1[i] - s2[i];
-		if (result != 0)
-			return (result);
-		i++;
-	}
-	return (result);
-}
-
-/**
- * _strcpy - copy string
- *
- * @dest: destination string arg
- *
- * @src: source string arg
- *
- * Return: destination string
- */
-
-
-char *_strcpy(char *dest, char *src)
-{
-	size_t j = 0;
-
-	while (src[j] != '\0')
-	{
-		dest[j] = src[j];
-		j++;
-	}
-	dest[j] = '\0';
-
-	return (dest);
-}
-
-/**
- * split_tok - splite string in array of words
- * @str: char  string
- * @delim: delimiter to separate words
- * Return: array of words
+ * split_tok - split string into tokens
+ * @str: string to split
+ * @delim: delimiter
+ * Return: pointer to array of tokens
  */
 
 char **split_tok(char *str, const char *delim)
 {
-	size_t i = 0, wc = 0, offset = 0, j = 0, k = 0;
+	size_t i = 0, wc = 0, off_set = 0, j = 0, k = 0;
 	char **array = NULL;
 
 	if (str == NULL)
@@ -95,29 +40,78 @@ char **split_tok(char *str, const char *delim)
 	}
 	for (i = 0; str[i] != '\0'; )
 	{
-		if ((str[i] == *delim || str[i] == '\t')
-				&& i == offset && str[i + 1] != '\n')
+		if (str[i + 1] != '\n' && i == off_set &&
+				(str[i] == '\t' || str[i] == *delim ))
 		{
-			i++, offset++;
+			i++, off_set++;
 				continue;
 		}
-		if (str[i] == *delim || str[i] == '\t' || str[i + 1] == '\0')
+		if (str[i] == '\t' || str[i + 1] == '\0' || str[i] == *delim)
 		{
-			if (offset == i && str[i + 1] == '\n')
+			if (str[i + 1] == '\n' && off_set == i)
 				break;
-			array[j] = malloc((i - offset + 2) * sizeof(char));
+			array[j] = malloc((i - off_set + 2) * sizeof(char));
 			if (array[j] == NULL)
 				return (NULL);
 			k = 0;
-			while (offset != i && (str[offset] != *delim || str[offset] != '\t'))
-				array[j][k++] = str[offset++];
+			while ((str[off_set] != '\t' || str[off_set] != *delim) && off_set != i)
+				array[j][k++] = str[off_set++];
 			array[j][k] = '\0';
-			offset++, j++;
-			if (str[offset] == '\n')
+			off_set++, j++;
+			if (str[off_set] == '\n')
 				break;
 		}
 		i++;
 	}
 	array[j] = NULL;
 	return (array);
+}
+
+
+/**
+ * _strcmp - compare two strings
+ * @s1: 1st arg - destination string
+ * @s2: 2nd arg - source string
+ * Return: 0 if strings are identical, else 1 or -1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int s1_length = 0, s2_length = 0, i = 0, res = 0, len = 0;
+
+	s1_length = _strlen(s1);
+	s2_length = _strlen(s2);
+	if (s1_length <= s2_length)
+		len = s1_length;
+	else
+		len = s2_length;
+	while (i <= len)
+	{
+		res = s1[i] - s2[i];
+		if (res != 0)
+			return (res);
+		i++;
+	}
+	return (res);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * @dest: destination string arg
+ * @src: source string arg
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	size_t i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
 }
