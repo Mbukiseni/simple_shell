91f166fad8f9cc4c35d7268a66fc601d01cf02bf
FahdAzouz <104470633+FahdAzouz@users.noreply.github.com>
2023-08-23 15:11:22 +0100

    1

--- getLine.c
+++ getLine.c
@@ -1,53 +1,57 @@
 #include "main.h"
 
 /**
- * _getline - getline fucn
- * @fstream: read file stream
+ * _getline - get line from file
+ * @fstream: file stream
  * @f: file
- * @f_len: file len
- * Return: number char printe
+ * @f_len: file length
+ * Return: number of characters read
  */
 
 ssize_t _getline(char **f, size_t *f_len, FILE *fstream)
 {
-	static ssize_t i;
-	char c = 'c', *buffer = malloc(*f_len + 1);
-	ssize_t getnum;
+	static ssize_t j;
+	ssize_t getnumber;
 	int count;
+	char c = 'c', *buffer = malloc(*f_len + 1);
 
 	if (buffer == NULL)
+	{
 		return (-1);
-	if (i == 0)
+	}
+	if (j == 0)
+	{
 		fflush(fstream);
+	}
 	else
 		return (-1);
 
 	buffer[*f_len] = '\0';
-	i = 0;
+	j = 0;
 	while (c != '\n')
 	{
 		count = read(STDIN_FILENO, &c, 1024);
-		if ((count == 0 && i == 0) || count == -1)
+		if (count == -1 || (count == 0 && j == 0))
 		{
 			free(buffer);
 			return (-1);
 		}
-		if (i >= 32)
-			buffer = _realloc(buffer, i, i + 32);
-
-		if (c != EOF && c != '\n')
+		if (j >= 32)
 		{
-			buffer[i] = c;
-			i++;
+			buffer = _realloc(buffer, j, j + 32);
+		}
+		if (c != '\n' && c != EOF)
+		{
+			buffer[j] = c;
+			j++;
 			continue;
 		}
-		buffer[i++] = '\n';
-		buffer[i] =  '\0';
-		copyPtr(f, f_len, buffer, i);
-		getnum = i;
+		buffer[j++] = '\n';
+		buffer[j] =  '\0';
+		copyPtr(f, f_len, buffer, j);
+		getnumber = j;
 	}
 	if (count != 0)
-		i = 0;
-	return (getnum);
-
+		j = 0;
+	return (getnumber);
 }
